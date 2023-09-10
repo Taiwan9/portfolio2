946097417f032c9ceb773937e672b89df5a5227c
Taiwan marinho <clownser1994@gmail.com>
2023-09-10 14:27:59 -0300

    fix: rodape Whatsapp Update

--- src/app/components/landing/rodape/RedeSociais.tsx
+++ src/app/components/landing/rodape/RedeSociais.tsx
@@ -4,7 +4,7 @@ import RedeSocial from "./RedeSocial"
 export default function RedesSociais() {
     return (
         <div className="flex">
-            <RedeSocial icone={<IconBrandWhatsapp />} url="https://web.whatsapp.com/send?phone=55219779342465" />
+            <RedeSocial icone={<IconBrandWhatsapp />} url="https://api.whatsapp.com/send?phone=5521979342465" />
             <RedeSocial icone={<IconBrandLinkedin />} url="https://www.linkedin.com/in/taiwan-marinho-de-carvalho-313116110/" />
             <RedeSocial icone={<IconBrandGithub />} url="https://github.com/Taiwan9" />
             <RedeSocial icone={<IconBrandFacebook />} url="https://www.facebook.com/taiwan.marinho.7/" />
